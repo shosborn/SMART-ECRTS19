7a6da42d0ab39013fcf905249dd0b42c5014d119
Joshua Bakita <bakitajoshua@gmail.com>
2019-02-08 05:19:04 -0500

    Update data and a new test script

--- run_4-32_100_mixed_stdev.sh
+++ run_4-32_100_mixed_stdev.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-./RunTests.py 4 100 /playpen/jbakita/4_100_1_normal.txt &
-./RunTests.py 8 100 /playpen/jbakita/8_100_1_normal.txt &
-./RunTests.py 16 100 /playpen/jbakita/16_100_1_normal.txt &
-./RunTests.py 32 100 /playpen/jbakita/32_100_1_normal.txt &
+./RunTests.py 4 100 4_100_1_normal.txt &
+./RunTests.py 8 100 8_100_1_normal.txt &
+./RunTests.py 16 100 16_100_1_normal.txt &
+./RunTests.py 32 100 32_100_1_normal.txt &
 
-./RunTests.py 4 100 /playpen/jbakita/4_100_1_2_normal.txt 0 1 2 &
-./RunTests.py 8 100 /playpen/jbakita/8_100_1_2_normal.txt 0 1 2 &
-./RunTests.py 16 100 /playpen/jbakita/16_100_1_2_normal.txt 0 1 2 &
-./RunTests.py 32 100 /playpen/jbakita/32_100_1_2_normal.txt 0 1 2 &
+./RunTests.py 4 100 4_100_1_2_normal.txt 0 1 2 &
+./RunTests.py 8 100 8_100_1_2_normal.txt 0 1 2 &
+./RunTests.py 16 100 16_100_1_2_normal.txt 0 1 2 &
+./RunTests.py 32 100 32_100_1_2_normal.txt 0 1 2 &
 
-./RunTests.py 4 100 /playpen/jbakita/4_100_2_1_normal.txt 0 2 1 &
-./RunTests.py 8 100 /playpen/jbakita/8_100_2_1_normal.txt 0 2 1 &
-./RunTests.py 16 100 /playpen/jbakita/16_100_2_1_normal.txt 0 2 1 &
-./RunTests.py 32 100 /playpen/jbakita/32_100_2_1_normal.txt 0 2 1 &
+./RunTests.py 4 100 4_100_2_1_normal.txt 0 2 1 &
+./RunTests.py 8 100 8_100_2_1_normal.txt 0 2 1 &
+./RunTests.py 16 100 16_100_2_1_normal.txt 0 2 1 &
+./RunTests.py 32 100 32_100_2_1_normal.txt 0 2 1 &
 
-./RunTests.py 4 100 /playpen/jbakita/4_100_2_2_normal.txt 0 2 2 &
-./RunTests.py 8 100 /playpen/jbakita/8_100_2_2_normal.txt 0 2 2 &
-./RunTests.py 16 100 /playpen/jbakita/16_100_2_2_normal.txt 0 2 2 &
-./RunTests.py 32 100 /playpen/jbakita/32_100_2_2_normal.txt 0 2 2 &
+./RunTests.py 4 100 4_100_2_2_normal.txt 0 2 2 &
+./RunTests.py 8 100 8_100_2_2_normal.txt 0 2 2 &
+./RunTests.py 16 100 16_100_2_2_normal.txt 0 2 2 &
+./RunTests.py 32 100 32_100_2_2_normal.txt 0 2 2 &
