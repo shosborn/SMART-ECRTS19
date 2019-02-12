18be26605fe2c5703067424316614c8dcff54087
Joshua Bakita <bakitajoshua@gmail.com>
2019-02-11 21:37:32 -0500

    Attempt to clean up parameter handling in the runner script

--- gaussian-average/run_4-32_mixed_stdev.sh
+++ gaussian-average/run_4-32_mixed_stdev.sh
@@ -15,51 +15,51 @@ mkdir -p results
 
 # All permutations of stdev 1 on s with [1,3] for f
 
-./RunTests.py 4 $1 results/4_$1_1_normal.txt &
-./RunTests.py 8 $1 results/8_$1_1_normal.txt &
-./RunTests.py 16 $1 results/16_$1_1_normal.txt &
-./RunTests.py 32 $1 results/32_$1_1_normal.txt &
+./RunTests.py 4 ${1} results/4_${1}_1_normal.txt &
+./RunTests.py 8 ${1} results/8_${1}_1_normal.txt &
+./RunTests.py 16 ${1} results/16_${1}_1_normal.txt &
+./RunTests.py 32 ${1} results/32_${1}_1_normal.txt &
 
-./RunTests.py 4 $1 results/4_$1_1_2_normal.txt 0 1 2 &
-./RunTests.py 8 $1 results/8_$1_1_2_normal.txt 0 1 2 &
-./RunTests.py 16 $1 results/16_$1_1_2_normal.txt 0 1 2 &
-./RunTests.py 32 $1 results/32_$1_1_2_normal.txt 0 1 2 &
+./RunTests.py 4 ${1} results/4_${1}_1_2_normal.txt 0 1 2 &
+./RunTests.py 8 ${1} results/8_${1}_1_2_normal.txt 0 1 2 &
+./RunTests.py 16 ${1} results/16_${1}_1_2_normal.txt 0 1 2 &
+./RunTests.py 32 ${1} results/32_${1}_1_2_normal.txt 0 1 2 &
 
-./RunTests.py 4 $1 results/4_$1_1_3_normal.txt 0 1 3 &
-./RunTests.py 8 $1 results/8_$1_1_3_normal.txt 0 1 3 &
-./RunTests.py 16 $1 results/16_$1_1_3_normal.txt 0 1 3 &
-./RunTests.py 32 $1 results/32_$1_1_3_normal.txt 0 1 3 &
+./RunTests.py 4 ${1} results/4_${1}_1_3_normal.txt 0 1 3 &
+./RunTests.py 8 ${1} results/8_${1}_1_3_normal.txt 0 1 3 &
+./RunTests.py 16 ${1} results/16_${1}_1_3_normal.txt 0 1 3 &
+./RunTests.py 32 ${1} results/32_${1}_1_3_normal.txt 0 1 3 &
 
 # All permutations of stdev 2 on s with [1,3] for f
 
-./RunTests.py 4 $1 results/4_$1_2_1_normal.txt 0 2 1 &
-./RunTests.py 8 $1 results/8_$1_2_1_normal.txt 0 2 1 &
-./RunTests.py 16 $1 results/16_$1_2_1_normal.txt 0 2 1 &
-./RunTests.py 32 $1 results/32_$1_2_1_normal.txt 0 2 1 &
+./RunTests.py 4 ${1} results/4_${1}_2_1_normal.txt 0 2 1 &
+./RunTests.py 8 ${1} results/8_${1}_2_1_normal.txt 0 2 1 &
+./RunTests.py 16 ${1} results/16_${1}_2_1_normal.txt 0 2 1 &
+./RunTests.py 32 ${1} results/32_${1}_2_1_normal.txt 0 2 1 &
 
-./RunTests.py 4 $1 results/4_$1_2_2_normal.txt 0 2 2 &
-./RunTests.py 8 $1 results/8_$1_2_2_normal.txt 0 2 2 &
-./RunTests.py 16 $1 results/16_$1_2_2_normal.txt 0 2 2 &
-./RunTests.py 32 $1 results/32_$1_2_2_normal.txt 0 2 2 &
+./RunTests.py 4 ${1} results/4_${1}_2_2_normal.txt 0 2 2 &
+./RunTests.py 8 ${1} results/8_${1}_2_2_normal.txt 0 2 2 &
+./RunTests.py 16 ${1} results/16_${1}_2_2_normal.txt 0 2 2 &
+./RunTests.py 32 ${1} results/32_${1}_2_2_normal.txt 0 2 2 &
 
-./RunTests.py 4 $1 results/4_$1_2_3_normal.txt 0 2 3 &
-./RunTests.py 8 $1 results/8_$1_2_3_normal.txt 0 2 3 &
-./RunTests.py 16 $1 results/16_$1_2_3_normal.txt 0 2 3 &
-./RunTests.py 32 $1 results/32_$1_2_3_normal.txt 0 2 3 &
+./RunTests.py 4 ${1} results/4_${1}_2_3_normal.txt 0 2 3 &
+./RunTests.py 8 ${1} results/8_${1}_2_3_normal.txt 0 2 3 &
+./RunTests.py 16 ${1} results/16_${1}_2_3_normal.txt 0 2 3 &
+./RunTests.py 32 ${1} results/32_${1}_2_3_normal.txt 0 2 3 &
 
 # All permutations of stdev 3 on s with [1,3] for f
 
-./RunTests.py 4 $1 results/4_$1_3_1_normal.txt 0 3 1 &
-./RunTests.py 8 $1 results/8_$1_3_1_normal.txt 0 3 1 &
-./RunTests.py 16 $1 results/16_$1_3_1_normal.txt 0 3 1 &
-./RunTests.py 32 $1 results/32_$1_3_1_normal.txt 0 3 1 &
+./RunTests.py 4 ${1} results/4_${1}_3_1_normal.txt 0 3 1 &
+./RunTests.py 8 ${1} results/8_${1}_3_1_normal.txt 0 3 1 &
+./RunTests.py 16 ${1} results/16_${1}_3_1_normal.txt 0 3 1 &
+./RunTests.py 32 ${1} results/32_${1}_3_1_normal.txt 0 3 1 &
 
-./RunTests.py 4 $1 results/4_$1_3_2_normal.txt 0 3 2 &
-./RunTests.py 8 $1 results/8_$1_3_2_normal.txt 0 3 2 &
-./RunTests.py 16 $1 results/16_$1_3_2_normal.txt 0 3 2 &
-./RunTests.py 32 $1 results/32_$1_3_2_normal.txt 0 3 2 &
+./RunTests.py 4 ${1} results/4_${1}_3_2_normal.txt 0 3 2 &
+./RunTests.py 8 ${1} results/8_${1}_3_2_normal.txt 0 3 2 &
+./RunTests.py 16 ${1} results/16_${1}_3_2_normal.txt 0 3 2 &
+./RunTests.py 32 ${1} results/32_${1}_3_2_normal.txt 0 3 2 &
 
-./RunTests.py 4 $1 results/4_$1_3_3_normal.txt 0 3 3 &
-./RunTests.py 8 $1 results/8_$1_3_3_normal.txt 0 3 3 &
-./RunTests.py 16 $1 results/16_$1_3_3_normal.txt 0 3 3 &
-./RunTests.py 32 $1 results/32_$1_3_3_normal.txt 0 3 3 &
+./RunTests.py 4 ${1} results/4_${1}_3_3_normal.txt 0 3 3 &
+./RunTests.py 8 ${1} results/8_${1}_3_3_normal.txt 0 3 3 &
+./RunTests.py 16 ${1} results/16_${1}_3_3_normal.txt 0 3 3 &
+./RunTests.py 32 ${1} results/32_${1}_3_3_normal.txt 0 3 3 &
