b22a5d61eaefcb7958ec6c8c96cbec54feb3119f
Joshua Bakita <bakitajoshua@gmail.com>
2019-02-17 15:42:42 -0500

    Fixup uniform-normal runner script

--- uniform-normal/run_4-32.sh
+++ uniform-normal/run_4-32.sh
@@ -12,55 +12,55 @@ fi
 mkdir -p results2
 
 # Try many epsilons
-declare -a epsilons={"01" "055" "1"}
+declare -a epsilons=("01" "055" "1")
 
 for e in "${epsilons[@]}"
 do
     # Sets with s (.65,1) and all f
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_65_1_65_1_normal 0 0.65 1 0.65 1 ${e} &
 
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_65_1_75_1_normal 0 0.65 1 0.75 1 ${e} &
 
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_65_1_85_1_normal 0 0.65 1 0.85 1 ${e} &
 
     # Sets with s (.75,1) and all f
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_75_1_65_1_normal 0 0.75 1 0.65 1 ${e} &
 
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_75_1_75_1_normal 0 0.75 1 0.75 1 ${e} &
 
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_75_1_85_1_normal 0 0.75 1 0.85 1 ${e} &
 
     # Sets with s (.85,1) and all f
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_85_1_65_1_normal 0 0.85 1 0.65 1 ${e} &
 
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_85_1_75_1_normal 0 0.85 1 0.75 1 ${e} &
 
-    python3 RunTests.py 4 ${1} 0.${e} results2/4_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 &
-    python3 RunTests.py 8 ${1} 0.${e} results2/8_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 &
-    python3 RunTests.py 16 ${1} 0.${e} results2/16_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 &
-    python3 RunTests.py 32 ${1} 0.${e} results2/32_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 &
+    echo RunTests.py 4 ${1} results2/4_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 ${e} &
+    echo RunTests.py 8 ${1} results2/8_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 ${e} &
+    echo RunTests.py 16 ${1} results2/16_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 ${e} &
+    echo RunTests.py 32 ${1} results2/32_${1}_${e}_85_1_85_1_normal 0 0.85 1 0.85 1 ${e} &
 done
